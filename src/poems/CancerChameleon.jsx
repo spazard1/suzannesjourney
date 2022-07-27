85396f331956dceded1cb05d13f80a368d4e9146
Steven Yackel <spazard1@gmail.com>
2022-07-26 20:54:00 -0500

    chameleon

---
+++ src/poems/CancerChameleon.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import TextContent from "../TextContent";
+
+import content from "./cancerIsAchameleon.txt";
+
+const Content = () => {
+  return <TextContent title="Cancer is a Chameleon" date="August 19, 2018" messageLocation={content} />;
+}
+
+export default Content;
