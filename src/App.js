60f15d7b2b390d2b11f0da9eb5d84943c597c217
Steven Yackel <styackel@microsoft.com>
2022-07-27 16:08:25 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect, useMemo, useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const ToBeWell = lazy(() => import("./poems/ToBeWell.jsx"));
-  const CancerChameleon = lazy(() => import("./poems/CancerChameleon.jsx"));
-  const CantDieYet = lazy(() => import("./poems/CantDieYet.jsx"));
-  const RubADubDub = lazy(() => import("./poems/RubADubDub.jsx"));
-  const TheresASharkInMyBathtub = lazy(() => import("./poems/TheresASharkInMyBathtub.jsx"));
 
+  const itemsList = useMemo(() => {
+    return {
+      Poems: {
+      "To Be Well" : "ToBeWell",
+      "Cancer is a Chameleon": "CancerChameleon", 
+      "Can't Die Yet": "CantDieYet",
+      "Rub A Dub Dub": "RubADubDub",
+      "There's a Shark In My Bathtub": "TheresASharkInMyBathtub"
+      },
+      Stories: {
+        "Little Toe": "LittleToe"
+      }
+    }
+  }, []);
 
-  const LittleToe = lazy(() => import("./stories/LittleToe.jsx"));
+  const [lazyElements, setLazyElements] = useState({});
 
-  const StJohnsGala = lazy(() => import("./videos/StJohnsGala.jsx"));
+  useEffect(() => {
+    const newLazyElements = {};
+    for (const [itemKey, item] of Object.entries(itemsList)) {
+      for (const [titleKey, titleFileName] of Object.entries(item)) {
+        newLazyElements[titleKey] = lazy(() => import("./" + itemKey.toLowerCase() + "/" + titleFileName + ".jsx"));
+      }
+    }
+
+    setLazyElements(newLazyElements);
+  }, [itemsList])
 
   return (
     <div>
@@ -24,43 +42,38 @@ function App() {
          <Container>
           <Nav>
               <Nav.Link href="/">Home</Nav.Link>
-              <NavDropdown title="Poems" menuVariant="dark">
-                <NavDropdown.Item href="/theresASharkInMyBathtub">There's A Shark In My Bathtub!</NavDropdown.Item>
-                <NavDropdown.Item href="/toBeWell">To Be Well</NavDropdown.Item>
-                <NavDropdown.Item href="/cancerChameleon">Cancer is a Chameleon</NavDropdown.Item>
-                <NavDropdown.Item href="/cantDieYet">Can't Die Yet</NavDropdown.Item>
-                <NavDropdown.Item href="/rubADubDub">Rub A Dub Dub</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Stories" menuVariant="dark">
-                <NavDropdown.Item href="/littleToe">Little Toe</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="Videos" menuVariant="dark">
-                <NavDropdown.Item href="/stJohnsGala">St. John's Gala</NavDropdown.Item>
-              </NavDropdown>
+              {Object.keys(itemsList).map(item => 
+                <NavDropdown key={item} title={item} menuVariant="dark">
+                  {Object.keys(itemsList[item]).map(titleKey => 
+                    <NavDropdown.Item key={titleKey} href={"/" + itemsList[item][titleKey]}>{titleKey}</NavDropdown.Item>
+                    )}
+                </NavDropdown>
+              )}
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="mainTextContainer">
-        <BrowserRouter>
-          <Suspense>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/littleToe" element={<LittleToe />} />
-              
-              <Route path="/toBeWell" element={<ToBeWell />} />
-              <Route path="/cancerChameleon" element={<CancerChameleon />} />
-              <Route path="/cantDieYet" element={<CantDieYet />} />
-              <Route path="/rubADubDub" element={<RubADubDub />} />
-              <Route path="/theresASharkInMyBathtub" element={<TheresASharkInMyBathtub />} />
-
-              <Route path="/stJohnsGala" element={<StJohnsGala />} />
-
-              <Route path="*" element={<Navigate replace to="/" />} />
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-      </div>
+      {lazyElements &&
+        <div className="mainTextContainer">
+          <BrowserRouter>
+            <Suspense>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                {Object.keys(itemsList).map(item => 
+                  (Object.keys(itemsList[item]).map(titleKey => {
+                      if (!(titleKey in lazyElements)) {
+                        return null;
+                      }
+                      const RouteHelper = lazyElements[titleKey];
+                      return <Route key={titleKey} path={"/" + itemsList[item][titleKey]} element={<RouteHelper />} />
+                    }
+                  ))
+                )}
+              </Routes>
+            </Suspense>
+          </BrowserRouter>
+        </div>
+      }
     </div>
   );
 }
