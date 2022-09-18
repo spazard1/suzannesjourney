4b2d55642e2868a47289b29c6753915bc5480c7e
Steven Yackel <spazard1@gmail.com>
2022-09-17 21:17:53 -0500

    adds tobewell date

--- src/poems/ToBeWell.jsx
+++ src/poems/ToBeWell.jsx
@@ -5,7 +5,7 @@ import TextContent from "../TextContent";
 import content from "./ToBeWell.txt";
 
 const Content = () => {
-  return <TextContent title="To Be Well" messageLocation={content} />;
+  return <TextContent title="To Be Well" date="May 19, 2017" messageLocation={content} />;
 }
 
 export default Content;
