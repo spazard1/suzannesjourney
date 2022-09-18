0543047dc77a78377f31cbc02d38240e1f291d12
Steven Yackel <spazard1@gmail.com>
2022-09-17 22:49:45 -0500

    remove nelsons

--- src/stories/Nelsons1994ChristmasLetter.jsx
+++ src/stories/Nelsons1994ChristmasLetter.jsx
@@ -5,7 +5,7 @@ import TextContent from "../TextContent";
 import content from "./Nelsons1994ChristmasLetter.txt";
 
 const Content = () => {
-  return <TextContent title="Nelson's 1994 Christmas Letter" date="December 1994" messageLocation={content} />;
+  return <TextContent title="1994 Christmas Letter" date="December 1994" messageLocation={content} />;
 }
 
 export default Content;
