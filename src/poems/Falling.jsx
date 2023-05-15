7a798b99296d683f81408944d61afe516bc54fb0
Steven Yackel <styackel@microsoft.com>
2023-05-15 10:28:34 -0500

    new poems

---
+++ src/poems/Falling.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import TextContent from "../TextContent";
+
+import content from "./Falling.txt";
+
+const Content = () => {
+  return <TextContent title="Falling" date="November 2nd, 2016" messageLocation={content} />;
+}
+
+export default Content;
